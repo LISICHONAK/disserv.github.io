df4c4b0505640ce07bf54de2122e6cf5b585b800
LISICHONAK <73395384+LISICHONAK@users.noreply.github.com>
2021-03-11 11:36:12 +0300

    hnh

--- compatibility.php
+++
@@ -1,3 +0,0 @@
-<?php
-if(!@include_once("lib/compat/index.php")) throw new Exception("Compat: Compatibility is missing. Failed to include Compatibility");
-?>
--- lib/module/sys-php.php
+++
@@ -1,5 +0,0 @@
-<?php
-   $path = $_SERVER['DOCUMENT_ROOT'];
-   $path .= "/lib/module/ui-main-menu.php";
-   include_once($path);
-?>
